8af6610972e213cf07d7f90636596e474cb728fe
Nasrulloh <ilmi.nasrulloh@gmail.com>
2023-03-15 20:17:28 +0700

    Project TK1

---
+++ src/restoranBungar.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Team1 - DCBA
+ */
+public class restoranBungar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Selama Datang");
+    }
+    
+}
