9bfb81c969dcf0aa5033990527fb165c3f6b92c9
unknown <nuruleramalia@gmail.com>
2023-03-16 23:56:57 +0700

    Amel

--- src/restoranBungar.java
+++ src/restoranBungar.java
@@ -5,9 +5,10 @@
  *         Aldo Sunan        - 
  *         M. Ilmi Nasrulloh - 2602233235
  *         Muliando Marpaung - 
- *         Nurul Amalia      - 
+ *         Nurul Amalia      - 2602243993
  *         Parlin Situmeang  - 2602239056
  */
+import java.util.Scanner;
 public class restoranBungar {
 
     /**
@@ -15,7 +16,26 @@ public class restoranBungar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Selama Datang");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Selamat siang..." + "\n");
+        System.out.print("Pesan untuk berapa orang :");
+        int jumlahorang = input.nextInt();
+        input.nextLine();
+        
+        System.out.println("Pesanan atas nama :");
+        String namapemesan = input.nextLine();
+        
+        
+        System.out.println("Menu spesial hari ini" + "\n");
+        System.out.println("=====================" + "\n");
+        
+        System.out.print("1. Nasi Goreng Spesial        @ Rp.9999.99" + "\n");
+        System.out.print("2. Ayam Bakar Spesial         @ Rp.12345.67" + "\n");
+        System.out.print("3. Steak Sirloin Spesial      @ Rp.21108.40" + "\n");
+        System.out.print("4. Kwetiaw Siram Spesial      @ Rp.13579.13" + "\n");
+        System.out.print("5. Kambing Guling Spesial     @ Rp.98765.43" + "\n");
+        
     }
     
 }
